9259c008b7567d9ecc487063b05b9f710e385f49
crashniels <39709384+crashniels@users.noreply.github.com>
2025-01-09 16:48:45 +0100

    Apply patches from 6.1

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -527,7 +527,8 @@ static void deallocate_vmid(struct device_queue_manager *dqm,
 				struct queue *q)
 {
 	/* On GFX v7, CP doesn't flush TC at dequeue */
-	if (q->device->adev->asic_type == CHIP_HAWAII)
+	if ((q->device->adev->asic_type == CHIP_HAWAII) ||
+		(q->device->adev->asic_type == CHIP_LIVERPOOL))
 		if (flush_texture_cache_nocpsch(q->device, qpd))
 			pr_err("Failed to flush TC\n");
 
@@ -2506,6 +2507,8 @@ struct device_queue_manager *device_queue_manager_init(struct kfd_node *dev)
 
 	switch (dev->adev->asic_type) {
 	/* HWS is not available on Hawaii. */
+	case CHIP_LIVERPOOL:
+	case CHIP_GLADIUS:
 	case CHIP_HAWAII:
 	/* HWS depends on CWSR for timely dequeue. CWSR is not
 	 * available on Tonga.
@@ -2575,6 +2578,8 @@ struct device_queue_manager *device_queue_manager_init(struct kfd_node *dev)
 	switch (dev->adev->asic_type) {
 	case CHIP_KAVERI:
 	case CHIP_HAWAII:
+	case CHIP_LIVERPOOL:
+	case CHIP_GLADIUS:
 		device_queue_manager_init_cik(&dqm->asic_ops);
 		break;
 
